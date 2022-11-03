83d1958763c1f13d8cca1618f1ada82ef7399645
Andrej Zerox <kitanovicandrej213@gmail.com>
2022-11-03 21:36:08 +0100

    feat: added bilda group

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 module.exports = (app) => {
 	const mailRotues = require('./mail');
-	const dreamcleanMailRotues = require('./dreamclean/mail');
+	const dreamcleanMailRoutes = require('./dreamclean/mail');
+	const bildagroupMailRoutes = require('./bildagroup/mail');
 
 	// API routes
 	app.use('/api/mail', mailRotues);
-	app.use('/api/dreamclean/mail', dreamcleanMailRotues);
+	app.use('/api/dreamclean/mail', dreamcleanMailRoutes);
+	app.use('/api/bildagroup/mail', bildagroupMailRoutes);
 };
---
+++ utils/bildagroup/mailer.js
@@ -0,0 +1,54 @@
+const mailjet = require('node-mailjet').connect(
+	process.env.BILDAGROUP_MJ_APIKEY_PUBLIC,
+	process.env.BILDAGROUP_MJ_APIKEY_PRIVATE
+);
+
+exports.getSingleEmail = async (id) => {
+	try {
+		const result = await mailjet.get('message', { version: 'v3' }).id(id).request();
+
+		return result.body;
+	} catch (err) {}
+};
+
+exports.getEmails = async () => {
+	try {
+		const result = await mailjet.get('message', { version: 'v3' }).request();
+
+		return result.body;
+	} catch (err) {}
+};
+
+exports.sendEmail = async ({ subject = '', html = '' }) => {
+	try {
+		await mailjet.post('send', { version: 'v3.1' }).request({
+			Messages: [
+				{
+					From: {
+						Email: 'info@putso.nu',
+						Name: 'Putso Website',
+					},
+					To: [
+						{
+							Email: 'info@putso.nu',
+							Name: 'Putso',
+						},
+						{
+							Email: 'kitanovicandrej213@gmail.com',
+							Name: 'Andrej [Debugging]',
+						},
+					],
+					Subject: subject,
+					HTMLPart: html,
+					CustomID: 'WindowCleaningBooking',
+				},
+			],
+		});
+
+		return 'Successfully sent!';
+	} catch (err) {
+		console.log(err);
+
+		return 'Error while sending!';
+	}
+};
