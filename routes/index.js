88eff74d46fd2d259239b178e81cf9e87f028f5c
Andrej Zerox <kitanovicandrej213@gmail.com>
2023-04-28 13:57:24 +0200

    feat: added mersol to routes

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ module.exports = (app) => {
 	const mailRotues = require('./mail');
 	const dreamcleanMailRoutes = require('./dreamclean/mail');
 	const bildagroupMailRoutes = require('./bildagroup/mail');
+	const mersolMailRoutes = require("./mersol/mail");
 
 	// API routes
 	app.use('/api/mail', mailRotues);
 	app.use('/api/dreamclean/mail', dreamcleanMailRoutes);
 	app.use('/api/bildagroup/mail', bildagroupMailRoutes);
+	app.use('/api/mersol/mail', mersolMailRoutes);
+
 };
